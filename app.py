b28c4e1801319f59bf3d05cb7a85165325c881b8
Poren Chiang <ren.chiang@gmail.com>
2016-08-05 19:16:29 +0800

    Heroku-ify script

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
+#!/usr/bin/env python3
 import os
-from bottle import request, route, run
+from bottle import Bottle, request, route, run
 
+# Config
 VERIFY_TOKEN = os.environ.get('JRF_VERIFY_TOKEN')
 
-@route('/hooks/messenger')
+# Variables
+app = Bottle()
+
+@app.route('/hooks/messenger')
 def messenger_hook():
     if request.query.get('hub.verify_token' == VERIFY_TOKEN):
         return request.query.get('hub.challenge')
@@ -11,4 +16,4 @@ def messenger_hook():
         return 'Procedure illegal'
 
 if __name__ = '__main__':
-    run(host='localhost', port=6000)
+    app.run()
