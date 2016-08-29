ebd580477629358d1b99db0a5958c4737dcd4e54
Poren Chiang <ren.chiang@gmail.com>
2016-08-29 17:24:26 +0800

    Remove sudo statement

--- standalone.sh
+++ standalone.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 source venv/bin/activate
-sudo -u www-data gunicorn app:app \
+gunicorn app:app \
     --log-file=production.log \
     --bind=unix:/tmp/bot.socket \
     --workers=1
