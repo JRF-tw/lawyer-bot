580b6d56908badd5b7e0bb0bbd36d49eeb6d0da4
Poren Chiang <ren.chiang@gmail.com>
2016-08-29 18:17:21 +0800

    Update standalone script

--- standalone.sh
+++ standalone.sh
@@ -2,5 +2,7 @@
 source venv/bin/activate
 gunicorn app:app \
     --log-file=production.log \
-    --bind=unix:/tmp/bot.socket \
-    --workers=1
+    --bind=unix:/var/run/gunicorn/jrf_bot.socket \
+    --workers=1 \
+    --pidfile=/var/run/gunicorn/jrf_bot.pid \
+    --daemon
