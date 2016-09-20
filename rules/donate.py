5800c3931b19486db50d3affe61e31a301c37938
billy3321 <billy3321@gmail.com>
2016-09-20 17:10:41 +0800

    fix elif

--- rules/donate.py
+++ rules/donate.py
@@ -8,9 +8,9 @@ class DonateRule(Rule):
     def run(self, message, **kwargs):
         if re.search('募款餐會', message):
             return '歡迎參加司改會於10月1日舉辦的募款餐會！請點此購買餐卷：\nhttps://donate.jrf.org.tw'
-        else if re.search('小額', message):
+        elif re.search('小額', message):
             return '歡迎定期定額贊助司改會！請點：\nhttps://www.jrf.org.tw/donate'
-        else if re.search('發票', message):
+        elif re.search('發票', message):
             return '司改會的電子發票代碼是「948」，諧音「救司法」，歡迎在結帳的時候使用，把發票寄給我們喔！'
         else
             return '歡迎捐款贊助司改會喔！請點：\nhttps://donate.jrf.org.tw'
