df2dbd93a254355c34f3b05a156cc7ca2004b1a6
billy3321 <billy3321@gmail.com>
2016-09-26 13:13:39 +0800

    try to make a beautiful reponse

--- rules/search.py
+++ rules/search.py
@@ -12,13 +12,37 @@ class SearchRule(Rule):
         try:
             response = requests.get('https://www.jrf.org.tw/api/articles', params={'query': keyword}).json()
             if response['status'] != 'success' or response['count'] < 1:
-                return '嗚呃，找不到你要問什麼！你可以試試看搜尋「{}」～'.format(random.choice([
+                return '嗚呃，找不到你要問什麼！你可以試試看對我說「我要問{}」～'.format(random.choice([
                     '司法改革', '大法官', '冤獄平反', '邱和順', '徐自強', '蘇建和', '司法陽光', '檢察官', '麵包'
                 ]))
             else:
                 article = random.choice(response['articles'])
                 author, _, occupation = article.get('author', '秘密人物').replace('文／', '').partition('／')
                 url = 'https://www.jrf.org.tw/articles/{}'.format(article['id'])
-                return '推薦一篇好文章〈{}〉，是由{}{}主筆的哦！\n{}'.format(article['title'], occupation, author, url)
+                text = '推薦一篇好文章〈{}〉，是由{}{}主筆的哦！\n{}'.format(article['title'], occupation, author, url)
+                result = {
+                    "text": text,
+                    "attachment": {
+                          "type": "template",
+                          "payload": {
+                            "template_type": "generic",
+                            "elements": [
+                                {
+                                    "title": article['title'],
+                                    "item_url": url,
+                                    "buttons": [
+                                    {
+                                        "type":"web_url",
+                                        "url": url,
+                                        "title":"看文章"
+                                    }]
+                                }
+                            ]
+                        }
+                    }
+                }
+                if article['image']:
+                    result["attachment"]["payload"]["elements"][0]["image_url"] = article['image']
+                return result
         except:
             return '嗚呃，網站在維修！'
